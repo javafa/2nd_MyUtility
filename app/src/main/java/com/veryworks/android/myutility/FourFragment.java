ac842d62c47924531b97ec16e2225c517e0047af
javafa <nigne2016@gmail.com>
2017-02-07 12:48:39 +0900

    Location Manager 추가

--- app/src/main/java/com/veryworks/android/myutility/FourFragment.java
+++ app/src/main/java/com/veryworks/android/myutility/FourFragment.java
@@ -1,7 +1,14 @@
 package com.veryworks.android.myutility;
 
 
+import android.*;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +16,6 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -23,6 +29,15 @@ public class FourFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap map;
     SupportMapFragment mapFragment;
+    MainActivity activity;
+    LocationManager manager;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        activity = (MainActivity) context;
+        manager = activity.getLocationManager();
+    }
 
     public FourFragment() {
         // Required empty public constructor
@@ -31,6 +46,30 @@ public class FourFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onResume() {
         super.onResume();
+        // 리스너 등록
+        try {
+            // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                    3000, // 통지사이의 최소 시간간격 (miliSecond) // 업데이트 간격
+                    10, // 통지사이의 최소 변경거리 (m)
+                    locationListener);
+            // 정보제공자로 네트워크프로바이더 등록
+//            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+//                    3000, // 통지사이의 최소 시간간격 (miliSecond)
+//                    10, // 통지사이의 최소 변경거리 (m)
+//                    locationListener);
+        } catch (SecurityException ex) {
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        // 리스너 해제
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        manager.removeUpdates(locationListener);
     }
 
     @Override
@@ -50,7 +89,41 @@ public class FourFragment extends Fragment implements OnMapReadyCallback {
         map = googleMap;
         LatLng sydney = new LatLng(-34, 151);
         map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15));
     }
 
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            double longitude = location.getLongitude(); //경도
+            double latitude = location.getLatitude();   //위도
+            double altitude = location.getAltitude();   //고도
+            float accuracy = location.getAccuracy();    //정확도
+            String provider = location.getProvider();   //위치제공자
+
+            // 내위치
+            LatLng myPosition = new LatLng(latitude, longitude);
+                                          // 위도,       경도
+            map.addMarker(new MarkerOptions().position(myPosition).title("I am here!"));
+                                                    //  내 위치             마커 타이틀
+            // 화면을 내위치로 이동시키는 함수
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 10));
+                                                             // 내 위치 , 줌레벨
+        }
+
+        @Override // provider 의 상태변경 시 호출
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override // gps 가 사용할 수 없었다가 사용가능해지면 호출
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override // gps 가 사용할 수 없을 때 호출
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 }
--- app/src/main/java/com/veryworks/android/myutility/MainActivity.java
+++ app/src/main/java/com/veryworks/android/myutility/MainActivity.java
@@ -1,12 +1,38 @@
 package com.veryworks.android.myutility;
 
+import android.*;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/*
+ * GPS 사용 순서
+ * 1. manifest 에 FINE, COARSE 권한추가
+ * 2. runtime permission 소스코드에 추가
+ * 3. GPS Location Manager 정의
+ * 4. GPS 가 켜져있는지 확인. 꺼져있다면 GPS 화면으로 이동
+ *
+ * 5. Listener 등록
+ * 6. Listener 실행
+ * 7. Listener 해제
+ */
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
     TwoFragment two;
     ThreeFragment three;
     FourFragment four;
+
+    // 위치정보 관리자
+    private LocationManager manager;
+    public LocationManager getLocationManager(){
+        return manager;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         three = new ThreeFragment();
         four = new FourFragment();
 
-        // 탭 Layout 정의
+        // - 탭 Layout 정의
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         // 탭 생성 및 타이틀 입력
         tabLayout.addTab( tabLayout.newTab().setText("계산기") );
@@ -36,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addTab( tabLayout.newTab().setText("검색") );
         tabLayout.addTab( tabLayout.newTab().setText("현재위치") );
 
-        // 프래그먼트 페이저 작성
+        // - 프래그먼트 페이저 작성
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         // 아답터 생성
         PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
@@ -47,6 +79,15 @@ public class MainActivity extends AppCompatActivity {
 
         // 2. 탭 리스너 : 탭이 변경되었을 때 페이지를 바꿔저는 리스너
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+
+        // - GPS 사용권한 체크
+        // 버전체크해서 마시말로우 보다 낮으면 런타임권한 체크를 하지 않는다
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        } else {
+            init();
+        }
+
     }
 
     class PagerAdapter extends FragmentStatePagerAdapter {
@@ -70,6 +111,92 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return TAB_COUNT;
+        }
+    }
+
+    private final int REQ_CODE = 100;
+
+    // 1. 권한체크
+    @TargetApi(Build.VERSION_CODES.M) // Target 지정 애너테이션
+    private void checkPermission(){
+        // 1.1 런타임 권한체크
+        if( checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                ){
+            // 1.2 요청할 권한 목록 작성
+            String permArr[] = {android.Manifest.permission.ACCESS_FINE_LOCATION
+                    , android.Manifest.permission.ACCESS_COARSE_LOCATION};
+            // 1.3 시스템에 권한요청
+            requestPermissions(permArr, REQ_CODE);
+        }else{
+            init();
+        }
+    }
+
+    // 2. 권한체크 후 콜백 < 사용자가 확인후 시스템이 호출하는 함수
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQ_CODE){
+            // 2.1 배열에 넘긴 런타임권한을 체크해서 승인이 됬으면
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                // 2.2 프로그램 실행
+                init();
+            }else{
+                Toast.makeText(this, "권한을 허용하지 않으시면 프로그램을 실행할 수 없습니다.", Toast.LENGTH_LONG).show();
+                // 선택 : 1 종료, 2 권한체크 다시 물어보기
+                finish();
+            }
+        }
+    }
+
+    private void init(){
+        // LocationManager 객체를 얻어온다
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // GPS_센서가 켜져있는지 확인
+        // 꺼져있다면 GPS 를 켜는 페이지로 이동
+        if(!gpsCheck()){
+            // - 팝업창 만들기
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            // 1. 팝업창 제목
+            alertDialog.setTitle("GPS 켜기");
+            // 2. 팝업창 메시지
+            alertDialog.setMessage("GPS 꺼져있습니다.\n 설정창으로 이동하시겠습니까?");
+            // 3. Yes 버튼 생성
+            alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog,int which) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            // 4. No 버튼 생성
+            alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            // 5. show 함수로 팝업창을 화면에 띄운다.
+            alertDialog.show();
+        }
+    }
+
+    // GPS 가 꺼져있는지 체크 롤리팝 이하버전...
+    private boolean gpsCheck(){
+        // 롤리팝 이상버전에서는 LocationManager로 GPS 꺼짐 여부 체크
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // 롤리팝 이하버전에서는 LOCATION_PROVIDERS_ALLOWED 로 체크
+        } else {
+            String gps = android.provider.Settings.Secure.getString(getContentResolver()
+                    , Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            if(gps.matches(".*gps.*")){
+                return true;
+            }else{
+                return false;
+            }
         }
     }
 }
